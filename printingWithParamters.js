01905c9c774a2d3775a5888827ce10be1408db8f
Jenna Woodley <jenatalie@msn.com>
2018-09-03 16:20:50 +0000

    printing with parameters

--- printingWithParamters.js
+++ printingWithParamters.js
@@ -1,10 +1,33 @@
+var https = require("https");
+
 function getAndPrintHTML (options) {
+ /* Add your code here */
 
-  /* Add your code here */
+  https.get(options, function(response){
 
-}
+  response.setEncoding('utf8');
 
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step3.html'
+  var newData =[];
+
+  response.on("data", function(data){
+   newData += data;
+
+  });
+
+  response.on("end", function(){
+    var buffered = Buffer.from(newData)
+     buffedData = buffered.toString();
+     console.log(buffedData);
+    //console.log("response stream complete.");
+  });
+
+  });
+
 };
+
+var options = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
+getAndPrintHTML(options);
